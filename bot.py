890b6f0f4878fbb1124f2f67119f97e165843370
Turner Vink <turner@turnervink.com>
2018-02-18 13:21:54 -0800

    Added analytics tracking

--- bot.py
+++ bot.py
@@ -37,9 +37,13 @@ async def on_ready():
 @client.event
 async def on_message(msg):
     if any(re.search(regex, msg.content.lower()) for regex in hotwords):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'thetits.png')
 
     if re.search(discrete_phrase('what if it was purple'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         attachment = None
 
         try:
@@ -67,45 +71,73 @@ async def on_message(msg):
                 await client.send_message(msg.channel, 'Looks like the last sent file isn\'t an image!')
 
     if re.search(discrete_phrase('sucks'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'thissucksman.png')
 
     if re.search(discrete_phrase('wack'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'wack.png')
 
     if re.search(discrete_phrase('boo'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'imright.png')
 
     if re.search(discrete_phrase('bird up'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'birdup.jpg')
 
     if re.search(discrete_phrase('rice'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'rice.png')
 
     if re.search('\\bhannibal (bustin\'?|busting) (thru|through)\\b', msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'hbt.jpg')
 
     if re.search(discrete_phrase('call me'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'callme.jpg')
 
     if re.search('(brb|be right back)', msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, './brb/' + random.choice(os.listdir('./brb')))
 
     if re.search(discrete_phrase('who killed hannibal?'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'death.png')
 
     if re.search('\\bburgers?\\b', msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'burgers.gif')
 
     if re.search(discrete_phrase('get yourself together'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'philly.jpg')
 
     if re.search(discrete_phrase('why would you say something so controversial yet so brave?'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'sobrave.gif')
 
     if any(re.search(regex, msg.content.lower()) for regex in wth):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'wth.gif')
 
     if re.search(discrete_phrase('bitch'), msg.content.lower()):
+        requests.post('https://hooks.zapier.com/hooks/catch/2977009/zwh2s3/', data={'serverId': str(msg.server.id), 'serverName': msg.server.name})
+
         await client.send_file(msg.channel, 'bitch.png')
 
 
